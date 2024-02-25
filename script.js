0fcb7c749a417c10228a1dc687b2f63ce038065f
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-02-26 01:13:00 +0600

    extend feature added

--- script.js
+++ script.js
@@ -4,6 +4,30 @@
 const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
+const extend  = document.querySelector('.extend');
+
+let currentProjects = 3;
+extend.addEventListener('click', (e)=>{
+  const projectList = [...document.querySelectorAll('.post li')];
+  e.target.classList.add('show-loader');
+
+  for(let i = currentProjects; i < currentProjects + 3; i++){
+    setTimeout(function(){
+      e.target.classList.remove('show-loader');
+      if(projectList[i]){
+        projectList[i].style.display = 'flex';        
+      }
+    }, )
+  }
+  currentProjects += 3;
+
+  //hide extend button after fully loaded
+  if(currentProjects >= projectList.length){
+    event.target.classList.add('loaded');
+  }
+
+});
+
 
 
 // navToggle function
